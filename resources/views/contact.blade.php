034af33f2d4153756c82a23383362a0f68cada69
NorahFernandez <norahfernandezglansbeek@gmail.com>
2024-09-05 19:06:57 +0200

    contact form
    Added a form to the contact pages so that people can send a message on the site

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,14 @@
+@extends('base')
+
+    @section('content')
+
+        <h1>Contact</h1>
+        <p>Here are some ways to contact me:</p>
+
+        <form action="">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+            <button type="submit">Email me</button>
+        </form>
+
+    @endsection
